b59f3aea5bb02f5be0681659b3dc4fc63d9c95cf
Glen <glen@Glens-MacBook-Pro-2.local>
2011-02-22 13:34:22 -0800

    copied delayed_job settings over from our current cookbook

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -3,7 +3,17 @@
 # Recipe:: default
 #
 
-if node[:instance_role] == "solo" || (node[:instance_role] == "util" && node[:name] !~ /^(mongodb|redis|memcache)/)
+# node[:instance_role]
+  # solo = Staging 1, 2
+  # util = Prod utility server
+  # app_master = Prod app master
+  # app = Prod app
+# node[:name]
+  # Memcached = Prod memcached server, '!~ /^(mongodb|redis|memcache|Memcache)/' will parse and not let it run on our memcached servers
+    # ex. 'Memcached' !~ /^(mongodb|redis|memcache|Memcache)/
+
+# Only run for Staging and Prod Util (non-memcache) Servers
+if node[:instance_role] == "solo" || (node[:instance_role] == "util" && node[:name] !~ /^(mongodb|redis|memcache|Memcache)/) #node[:instance_role] == "solo" || (node[:instance_role] == "util" && node[:name] !~ /^(mongodb|redis|memcache)/)
   node[:applications].each do |app_name,data|
   
     # determine the number of workers to run based on instance size
@@ -12,7 +22,7 @@ if node[:instance_role] == "solo" || (node[:instance_role] == "util" && node[:na
     else
       case node[:ec2][:instance_type]
       when 'm1.small': worker_count = 2
-      when 'c1.medium': worker_count = 4
+      when 'c1.medium': worker_count = 2 #4
       when 'c1.xlarge': worker_count = 8
       else 
         worker_count = 2
